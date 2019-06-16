c4bb6c58ea65b1369c2d01c69216b54ccd262d89
dzikadi <33896297+dzikadi@users.noreply.github.com>
2019-06-16 17:44:51 +0700

    Update line.php

--- line.php
+++ line.php
@@ -11,7 +11,7 @@ function send_LINE($msg){
       ];
 
       // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/push';
+      $url = 'https://api.line.me/v2/bot/message/broadcast';
       $data = [
 
         'to' => 'U15ddb8750c5577d813220f35acb70a0c',
