06a63e524f7d3412d9f29561289d3f83bbd060c8
dzikadi <33896297+dzikadi@users.noreply.github.com>
2019-06-16 13:44:47 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'dendihazikfuadi',
+        'to' => 'U15ddb8750c5577d813220f35acb70a0c',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
