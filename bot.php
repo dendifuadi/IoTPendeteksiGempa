e68de8dc73a8f87ba4a4615cda85bf9c81889800
dzikadi <33896297+dzikadi@users.noreply.github.com>
2019-06-16 16:38:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 
-			$Topic = "IotPendeteksiGempa" ;
+			$Topic = "Nodemcu" ;
 			getMqttfromlineMsg($Topic,$text);
 			   
 			
